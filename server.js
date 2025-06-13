059986f93f70563d5c6ba53f9e408f18f45167fc
portiaportia <portiaplante@gmail.com>
2025-06-13 12:55:44 -0400

    edit and delete

--- server.js
+++ server.js
@@ -123,6 +123,47 @@ app.post("/api/houses", upload.single("img") , (req, res)=>{
     res.status(200).send(house);
 });
 
+app.put("/api/houses/:id", upload.single("img"),(req,res)=>{
+    const house = houses.find((house)=>house._id===parseInt(req.params.id));
+
+    if(!house){
+        res.status(404).send("The house with the provided id was not found");
+        return;
+    }
+
+    const result = validateHouse(req.body);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    house.name = req.body.name;
+    house.description = req.body.description;
+    house.size = req.body.size;
+    house.bathrooms = req.body.bathrooms;
+    house.bedrooms = req.body.bedrooms;
+
+    if(req.file){
+        house.main_image = req.file.filename;
+    }
+
+    res.status(200).send(house);
+});
+
+app.delete("/api/houses/:id",(req,res)=>{
+    console.log("I'm trying to delete" + req.params.id);
+    const house = houses.find((house)=>house._id===parseInt(req.params.id));
+
+    if(!house){
+        res.status(404).send("The house with the provided id was not found");
+        return;
+    }
+    const index = houses.indexOf(house);
+    houses.splice(index,1);
+    res.status(200).send(house);
+});
+
 const validateHouse = (house) => {
     const schema = Joi.object({
         _id:Joi.allow(""),
